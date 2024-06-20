fddf2d3c98b8706755a1fed2607d521e5c1749db
jesper-hingelbjerg <132884548+jeshi-netdesign@users.noreply.github.com>
2024-06-20 09:37:16 +0200

    Update script.bat

--- CherwellGadget/Program/script.bat
+++ CherwellGadget/Program/script.bat
@@ -1,7 +1,4 @@
 @ECHO OFF
 set arg1=%1
-"C:\Program Files (x86)\Internet Explorer\iexplore.exe"
-@REM node "C:\Users\m91183\Desktop\cmdTest\index.js" %arg1%
-@REM "C:\Program Files (x86)\Cherwell Software\Cherwell Service Management\Trebuchet.App.exe" /s "Kunde" "XXX"
-@REM pause
+"C:\Program Files (x86)\Cherwell Software\Cherwell Service Management\Trebuchet.App.exe" /s "Kunde" %arg1%
 exit
